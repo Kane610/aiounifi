a792bb8b30c127657b2c52f8f632a5ef35c59bec
Robert Svensson <kane610@me.com>
2020-05-05 08:35:23 +0200

    Improve default return values and typing of client properties

--- aiounifi/clients.py
+++ aiounifi/clients.py
@@ -13,15 +13,15 @@ class Clients(APIItems):
 
     KEY = "mac"
 
-    def __init__(self, raw, request):
+    def __init__(self, raw: list, request) -> None:
         super().__init__(raw, request, URL, Client)
 
-    async def async_block(self, mac):
+    async def async_block(self, mac: str) -> None:
         """Block client from controller."""
         data = {"mac": mac, "cmd": "block-sta"}
         await self._request("post", URL_CLIENT_STATE_MANAGER, json=data)
 
-    async def async_unblock(self, mac):
+    async def async_unblock(self, mac: str) -> None:
         """Unblock client from controller."""
         data = {"mac": mac, "cmd": "unblock-sta"}
         await self._request("post", URL_CLIENT_STATE_MANAGER, json=data)
@@ -32,7 +32,7 @@ class ClientsAll(APIItems):
 
     KEY = "mac"
 
-    def __init__(self, raw, request):
+    def __init__(self, raw: dict, request) -> None:
         super().__init__(raw, request, URL_ALL, Client)
 
 
@@ -40,88 +40,88 @@ class Client(APIItem):
     """Represents a client network device."""
 
     @property
-    def blocked(self):
-        return self.raw.get("blocked", "")
+    def blocked(self) -> bool:
+        return self.raw.get("blocked", False)
 
     @property
-    def essid(self):
+    def essid(self) -> str:
         return self.raw.get("essid", "")
 
     @property
-    def hostname(self):
+    def hostname(self) -> str:
         return self.raw.get("hostname", "")
 
     @property
-    def ip(self):
+    def ip(self) -> str:
         return self.raw.get("ip", "")
 
     @property
-    def is_guest(self):
-        return self.raw.get("is_guest", "")
+    def is_guest(self) -> bool:
+        return self.raw.get("is_guest", False)
 
     @property
-    def is_wired(self):
-        return self.raw.get("is_wired", "")
+    def is_wired(self) -> bool:
+        return self.raw.get("is_wired")
 
     @property
-    def last_seen(self):
-        return self.raw.get("last_seen", "")
+    def last_seen(self) -> int:
+        return self.raw.get("last_seen")
 
     @property
-    def mac(self):
+    def mac(self) -> str:
         return self.raw.get("mac", "")
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.raw.get("name", "")
 
     @property
-    def oui(self):
+    def oui(self) -> str:
         return self.raw.get("oui", "")
 
     @property
-    def site_id(self):
+    def site_id(self) -> str:
         return self.raw.get("site_id", "")
 
     @property
-    def sw_depth(self):
-        return self.raw.get("sw_depth", "")
+    def sw_depth(self) -> int:
+        return self.raw.get("sw_depth")
 
     @property
-    def sw_mac(self):
+    def sw_mac(self) -> str:
         """MAC for switch client is connected to."""
         return self.raw.get("sw_mac", "")
 
     @property
-    def sw_port(self):
+    def sw_port(self) -> int:
         """Switch port client is connected to."""
-        return self.raw.get("sw_port", "")
+        return self.raw.get("sw_port")
 
     @property
-    def rx_bytes(self):
+    def rx_bytes(self) -> int:
         """Bytes received over wireless connection."""
         return self.raw.get("rx_bytes", 0)
 
     @property
-    def tx_bytes(self):
+    def tx_bytes(self) -> int:
         """Bytes transferred over wireless connection."""
         return self.raw.get("tx_bytes", 0)
 
     @property
-    def uptime(self):
+    def uptime(self) -> int:
         return self.raw.get("uptime", 0)
 
     @property
-    def wired_rx_bytes(self):
+    def wired_rx_bytes(self) -> int:
         """Bytes received over wired connection."""
         return self.raw.get("wired-rx_bytes", 0)
 
     @property
-    def wired_tx_bytes(self):
+    def wired_tx_bytes(self) -> int:
         """Bytes transferred over wired connection."""
         return self.raw.get("wired-tx_bytes", 0)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Return the representation."""
         name = self.name or self.hostname
         return f"<Client {name}: {self.mac} {self.raw}>"
