6c799ed76d5afbe15bdccc4440c5b9a5c85e9b0a
Robert Svensson <kane610@me.com>
2021-05-10 08:46:05 +0200

    Add additional github actions relese-drafter/test Add dependabot Specify requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 """Setup for aioUnifi"""
 
-# https://ubntwiki.com/products/software/unifi-controller/api
-# https://jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
-# http://peterdowns.com/posts/first-time-with-pypi.html
-# pip install -e .
-# Upload to PyPI Live
-# python setup.py sdist bdist_wheel
-# twine upload dist/aiounifi-* --skip-existing
-
 from setuptools import setup
 
 setup(
@@ -21,6 +13,7 @@ setup(
     url="https://github.com/Kane610/aiounifi",
     download_url="https://github.com/Kane610/aiounifi/archive/v26.tar.gz",
     install_requires=["aiohttp"],
+    tests_require=["pytest-asyncio", "pytest-aiohttp", "pytest", "aioresponses"],
     keywords=["unifi", "homeassistant"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
 )
