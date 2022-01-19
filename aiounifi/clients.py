c48fd39b3e424bf0d475cf563f054a0f2172fa2b
Robert Svensson <Kane610@users.noreply.github.com>
2022-01-19 17:27:11 +0100

    Add more client attributes (#100)
    * Add new attributes for clients
    Increase test coverage of clients
    
    * Add wired client fixture
    Improve client test
    
    * Make powersave_enabled return type Optional[bool]

--- aiounifi/clients.py
+++ aiounifi/clients.py
@@ -67,9 +67,9 @@ class Client(APIItem):
         return self.raw.get("ap_mac", "")
 
     @property
-    def association_time(self) -> Optional[int]:
+    def association_time(self) -> int:
         """When was client associated with controller."""
-        return self.raw.get("assoc_time")
+        return self.raw.get("assoc_time", 0)
 
     @property
     def blocked(self) -> bool:
@@ -87,9 +87,14 @@ class Client(APIItem):
         return self.raw.get("essid", "")
 
     @property
-    def first_seen(self) -> Optional[int]:
+    def firmware_version(self) -> str:
+        """Firmware version of client."""
+        return self.raw.get("fw_version", "")
+
+    @property
+    def first_seen(self) -> int:
         """When was client first seen."""
-        return self.raw.get("first_seen")
+        return self.raw.get("first_seen", 0)
 
     @property
     def fixed_ip(self) -> str:
@@ -100,6 +105,11 @@ class Client(APIItem):
     def hostname(self) -> str:
         """Hostname of client."""
         return self.raw.get("hostname", "")
+
+    @property
+    def idle_time(self) -> int:
+        """Idle time of client."""
+        return self.raw.get("idletime", 0)
 
     @property
     def ip(self) -> str:
@@ -117,14 +127,29 @@ class Client(APIItem):
         return self.raw.get("is_wired", False)
 
     @property
-    def last_seen(self) -> Optional[int]:
+    def last_seen(self) -> int:
         """When was client last seen."""
-        return self.raw.get("last_seen")
+        return self.raw.get("last_seen", 0)
 
     @property
-    def latest_association_time(self) -> Optional[int]:
+    def last_seen_by_access_point(self) -> int:
+        """When was client last seen by access point."""
+        return self.raw.get("_last_seen_by_uap", 0)
+
+    @property
+    def last_seen_by_gateway(self) -> int:
+        """When was client last seen by gateway."""
+        return self.raw.get("_last_seen_by_ugw", 0)
+
+    @property
+    def last_seen_by_switch(self) -> int:
+        """When was client last seen by network switch."""
+        return self.raw.get("_last_seen_by_usw", 0)
+
+    @property
+    def latest_association_time(self) -> int:
         """When was client last associated with controller."""
-        return self.raw.get("latest_assoc_time")
+        return self.raw.get("latest_assoc_time", 0)
 
     @property
     def mac(self) -> str:
@@ -140,6 +165,11 @@ class Client(APIItem):
     def oui(self) -> str:
         """Vendor string for client MAC."""
         return self.raw.get("oui", "")
+
+    @property
+    def powersave_enabled(self) -> Optional[bool]:
+        """Powersave functionality enabled for wireless client."""
+        return self.raw.get("powersave_enabled")
 
     @property
     def site_id(self) -> str:
@@ -187,6 +217,26 @@ class Client(APIItem):
         return self.raw.get("uptime", 0)
 
     @property
+    def uptime_by_access_point(self) -> int:
+        """Uptime of client observed by access point."""
+        return self.raw.get("_uptime_by_uap", 0)
+
+    @property
+    def uptime_by_gateway(self) -> int:
+        """Uptime of client observed by gateway."""
+        return self.raw.get("_uptime_by_ugw", 0)
+
+    @property
+    def uptime_by_switch(self) -> int:
+        """Uptime of client observed by network switch."""
+        return self.raw.get("_uptime_by_usw", 0)
+
+    @property
+    def wired_rate_mbps(self) -> int:
+        """Wired rate in Mbps."""
+        return self.raw.get("wired_rate_mbps", 0)
+
+    @property
     def wired_rx_bytes(self) -> int:
         """Bytes received over wired connection."""
         return self.raw.get("wired-rx_bytes", 0)
@@ -205,8 +255,3 @@ class Client(APIItem):
     def wired_tx_bytes_r(self) -> int:
         """Bytes recently transferred over wired connection."""
         return self.raw.get("wired-tx_bytes-r", 0)
-
-    def __repr__(self) -> str:
-        """Return the representation."""
-        name = self.name or self.hostname
-        return f"<Client {name}: {self.mac} {self.raw}>"
