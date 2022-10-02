325177fc01628f8ba32871aaa0f3f1060161206a
Robert Svensson <Kane610@users.noreply.github.com>
2022-10-02 19:47:12 +0200

    Bump to v36

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.9"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
-VERSION = "35"
+VERSION = "36"
 
 setup(
     name="aiounifi",
