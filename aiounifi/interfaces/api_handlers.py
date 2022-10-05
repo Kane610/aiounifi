4566e710d54ad9f74526153c54d38e99997bd6ee
Robert Svensson <Kane610@users.noreply.github.com>
2022-10-05 21:51:17 +0200

    Improve ApiHandlers subscription

--- aiounifi/interfaces/api_handlers.py
+++ aiounifi/interfaces/api_handlers.py
@@ -3,8 +3,9 @@
 from __future__ import annotations
 
 from collections.abc import Callable, ItemsView, Iterator, ValuesView
+import enum
 import logging
-from typing import TYPE_CHECKING, Any, Final, Generic, final
+from typing import TYPE_CHECKING, Any, Final, Generic, Optional, final
 
 from ..models import ResourceType
 from ..models.request_object import RequestObject
@@ -14,8 +15,20 @@ if TYPE_CHECKING:
     from ..models.event import Event, EventKey
     from ..models.message import Message, MessageKey
 
-SubscriptionType = Callable[[str, str], None]
+
+class ItemEvent(enum.Enum):
+    """The event action of the item."""
+
+    ADDED = "added"
+    CHANGED = "changed"
+    DELETED = "deleted"
+
+
+CallbackType = Callable[[ItemEvent, str], None]
+SubscriptionType = tuple[CallbackType, Optional[tuple[ItemEvent, ...]]]
 UnsubscribeType = Callable[[], None]
+
+ID_FILTER_ALL = "*"
 
 LOGGER = logging.getLogger(__name__)
 
@@ -37,7 +50,7 @@ class APIHandler(Generic[ResourceType]):
         """Initialize API items."""
         self.controller = controller
         self._items: dict[int | str, ResourceType] = {}
-        self._subscribers: list[SubscriptionType] = []
+        self._subscribers: dict[str, list[SubscriptionType]] = {ID_FILTER_ALL: []}
 
         if message_filter := self.process_messages + self.remove_messages:
             controller.messages.subscribe(self.process_message, message_filter)
@@ -89,9 +102,7 @@ class APIHandler(Generic[ResourceType]):
             return ""
 
         self._items[obj_id] = self.item_cls(raw, self.controller)
-
-        for callback in self._subscribers:
-            callback("added", obj_id)
+        self.signal_subscribers(ItemEvent.ADDED, obj_id)
 
         return obj_id
 
@@ -105,16 +116,48 @@ class APIHandler(Generic[ResourceType]):
             return obj_id
         return ""
 
-    def subscribe(self, callback: SubscriptionType) -> UnsubscribeType:
+    def signal_subscribers(self, event: ItemEvent, obj_id: str) -> None:
+        """Signal subscribers."""
+        subscribers: list[SubscriptionType] = (
+            self._subscribers.get(obj_id, []) + self._subscribers[ID_FILTER_ALL]
+        )
+        for callback, event_filter in subscribers:
+            if event_filter is not None and event not in event_filter:
+                continue
+            callback(event, obj_id)
+
+    def subscribe(
+        self,
+        callback: CallbackType,
+        event_filter: tuple[ItemEvent, ...] | ItemEvent | None = None,
+        id_filter: tuple[str] | str | None = None,
+    ) -> UnsubscribeType:
         """Subscribe to added events.
 
         "callback" - callback function to call when an event emits.
         Return function to unsubscribe.
         """
-        self._subscribers.append(callback)
+        if isinstance(event_filter, ItemEvent):
+            event_filter = (event_filter,)
+        subscription = (callback, event_filter)
+
+        _id_filter: tuple[str]
+        if id_filter is None:
+            _id_filter = (ID_FILTER_ALL,)
+        elif isinstance(id_filter, str):
+            _id_filter = (id_filter,)
+
+        subscription = (callback, event_filter)
+        for obj_id in _id_filter:
+            if obj_id not in self._subscribers:
+                self._subscribers[obj_id] = []
+            self._subscribers[obj_id].append(subscription)
 
         def unsubscribe() -> None:
-            self._subscribers.remove(callback)
+            for obj_id in _id_filter:
+                if obj_id not in self._subscribers:
+                    continue
+                self._subscribers[obj_id].remove(subscription)
 
         return unsubscribe
 
