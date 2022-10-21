25ce37547a7808d3283facd0695fc15f0867d4f4
Robert Svensson <Kane610@users.noreply.github.com>
2022-10-21 07:57:20 +0200

    Add more device variants to outlet tests

--- tests/test_outlets.py
+++ tests/test_outlets.py
@@ -5,10 +5,12 @@ pytest --cov-report term-missing --cov=aiounifi.outlets tests/test_outlets.py
 
 from unittest.mock import Mock
 
+import pytest
+
 from aiounifi.interfaces.api_handlers import ItemEvent
 from aiounifi.models.outlet import Outlet
 
-from .fixtures import STRIP_UP6
+from .fixtures import PDU_PRO, PLUG_UP1, STRIP_UP6
 
 
 async def test_handler_ports(unifi_controller):
@@ -92,21 +94,90 @@ async def test_handler_process_device_no_index(unifi_controller):
     assert len(ports.items()) == 0
 
 
-async def test_outlet(unifi_controller):
+data_test_outlet = [
+    {
+        "device": PDU_PRO,
+        "obj_id": "00:00:00:00:00:84_1",
+        "name": "USB Outlet 1",
+        "index": 1,
+        "relay_state": True,
+        "has_relay": None,
+        "cycle_enabled": False,
+        "has_metering": None,
+        "caps": 1,
+        "voltage": None,
+        "current": None,
+        "power": None,
+        "power_factor": None,
+        "repr": "<USB Outlet 1: relay state True>",
+    },
+    {
+        "device": PDU_PRO,
+        "obj_id": "00:00:00:00:00:84_5",
+        "name": "Console",
+        "index": 5,
+        "relay_state": True,
+        "has_relay": None,
+        "cycle_enabled": None,
+        "has_metering": None,
+        "caps": 3,
+        "voltage": "118.566",
+        "current": "0.061",
+        "power": "3.815",
+        "power_factor": "0.527",
+        "repr": "<Console: relay state True>",
+    },
+    {
+        "device": PLUG_UP1,
+        "obj_id": "fc:ec:da:76:4f:5f_1",
+        "name": "Outlet 1",
+        "index": 1,
+        "relay_state": False,
+        "has_relay": True,
+        "cycle_enabled": None,
+        "has_metering": False,
+        "caps": None,
+        "voltage": None,
+        "current": None,
+        "power": None,
+        "power_factor": None,
+        "repr": "<Outlet 1: relay state False>",
+    },
+    {
+        "device": STRIP_UP6,
+        "obj_id": "78:45:58:fc:16:7d_1",
+        "name": "Outlet 1",
+        "index": 1,
+        "relay_state": False,
+        "has_relay": True,
+        "cycle_enabled": False,
+        "has_metering": False,
+        "caps": None,
+        "voltage": None,
+        "current": None,
+        "power": None,
+        "power_factor": None,
+        "repr": "<Outlet 1: relay state False>",
+    },
+]
+
+
+@pytest.mark.parametrize("test_data", data_test_outlet)
+async def test_outlet(unifi_controller, test_data):
     """Verify that device outlet model works."""
-    unifi_controller.devices.process_raw([STRIP_UP6])
-    outlet = unifi_controller.outlets["78:45:58:fc:16:7d_1"]
+    unifi_controller.devices.process_raw([test_data["device"]])
+    outlet = unifi_controller.outlets[test_data["obj_id"]]
 
-    assert outlet.name == "Outlet 1"
-    assert outlet.index == 1
-    assert outlet.relay_state is False
-    assert outlet.has_relay is True
-    assert outlet.cycle_enabled is False
-    assert outlet.has_metering is False
-    assert outlet.caps is None
-    assert outlet.voltage is None
-    assert outlet.current is None
-    assert outlet.power is None
-    assert outlet.power_factor is None
+    assert outlet.name == test_data["name"]
+    assert outlet.index == test_data["index"]
+    assert outlet.relay_state == test_data["relay_state"]
+    assert outlet.has_relay == test_data["has_relay"]
+    assert outlet.cycle_enabled == test_data["cycle_enabled"]
+    assert outlet.has_metering == test_data["has_metering"]
+    assert outlet.caps == test_data["caps"]
+    assert outlet.voltage == test_data["voltage"]
+    assert outlet.current == test_data["current"]
+    assert outlet.power == test_data["power"]
+    assert outlet.power_factor == test_data["power_factor"]
 
-    assert repr(outlet) == "<Outlet 1: relay state False>"
+    assert repr(outlet) == test_data["repr"]
