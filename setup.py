4af0be22876b4a6d9328886923df17acc0528b3c
Robert Svensson <Kane610@users.noreply.github.com>
2023-03-24 23:16:37 +0100

    Bump to v45

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.10"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
-VERSION = "44"
+VERSION = "45"
 
 setup(
     name="aiounifi",
