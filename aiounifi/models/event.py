74f9d1fb839b1e7f7b981b505161c88ecff7be38
Jelger Haanstra <jghaanstra@users.noreply.github.com>
2023-04-24 20:58:40 +0200

    Attempt to fix EVT_HS_AuthedByPassword warnings in HA log (#358)
    * Fix EVT_HS_AuthedByPassword warnings in log
    
    * Update aiounifi/models/event.py
    
    ---------
    
    Co-authored-by: Robert Svensson <Kane610@users.noreply.github.com>

--- aiounifi/models/event.py
+++ aiounifi/models/event.py
@@ -96,6 +96,7 @@ class EventKey(enum.Enum):
     AD_LOGIN = "EVT_AD_Login"
     AD_SCHEDULE_UPGRADE_FAILED_NOT_FOUND = "EVT_AD_ScheduleUpgradeFailedNotFound"
 
+    HOT_SPOT_AUTHED_BY_PASSWORD = "EVT_HS_AuthedByPassword"
     HOT_SPOT_VOUCHER_USED = "EVT_HS_VoucherUsed"
 
     UNKNOWN = "unknown"
