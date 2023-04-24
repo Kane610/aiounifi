886d306fdc1c2f23f77a6b21f3da64287b09fe36
Robert Svensson <Kane610@users.noreply.github.com>
2023-04-24 17:41:59 +0200

    Add event EVT_DM_Connected (#363)

--- aiounifi/models/event.py
+++ aiounifi/models/event.py
@@ -37,6 +37,7 @@ class EventKey(enum.Enum):
     ACCESS_POINT_UPGRADE_SCHEDULED = "EVT_AP_UpgradeScheduled"
     ACCESS_POINT_UPGRADED = "EVT_AP_Upgraded"
 
+    DREAM_MACHINE_CONNECTED = "EVT_DM_Connected"
     DREAM_MACHINE_LOST_CONTACT = "EVT_DM_Lost_Contact"
 
     GATEWAY_ADOPTED = "EVT_GW_Adopted"
