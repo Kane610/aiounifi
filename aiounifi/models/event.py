e37ac2fbd3c79f112828668a55778f58b7eada11
Nerdix <70015952+N3rdix@users.noreply.github.com>
2023-07-14 08:55:37 +0200

    Add event EVT_AP_UpgradeFailed

--- aiounifi/models/event.py
+++ aiounifi/models/event.py
@@ -35,6 +35,7 @@ class EventKey(enum.Enum):
     ACCESS_POINT_RESTARTED = "EVT_AP_Restarted"
     ACCESS_POINT_RESTARTED_UNKNOWN = "EVT_AP_RestartedUnknown"
     ACCESS_POINT_UPGRADE_SCHEDULED = "EVT_AP_UpgradeScheduled"
+    ACCESS_POINT_UPGRADE_FAILED = "EVT_AP_UpgradeFailed"
     ACCESS_POINT_UPGRADED = "EVT_AP_Upgraded"
 
     DREAM_MACHINE_CONNECTED = "EVT_DM_Connected"
