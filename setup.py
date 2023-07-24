9405ea5b5c1a131222a98d0a488920827c9ead30
Robert Svensson <Kane610@users.noreply.github.com>
2023-07-24 21:23:23 +0200

    Bump to v50

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.10"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
-VERSION = "49"
+VERSION = "50"
 
 setup(
     name="aiounifi",
