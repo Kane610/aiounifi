5ab699ff413744622c64781f9200fae34292320e
Robert Svensson <Kane610@users.noreply.github.com>
2023-08-21 22:46:48 +0200

    Replace setup.py and requirements-test

--- setup.sh
+++ setup.sh
@@ -9,5 +9,4 @@ cd "$(dirname "$0")"
 python3 -m venv venv
 source venv/bin/activate
 
-python3 -m pip install -r requirements.txt -r requirements-test.txt
-pre-commit install
+python3 -m pip install -r requirements.txt .[test] .[dev]
