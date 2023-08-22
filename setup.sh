d50755eb7bc4e24fd21e4c9907e7db69fc8638fb
Robert Svensson <Kane610@users.noreply.github.com>
2023-08-22 07:45:04 +0200

    Improve naming Add forum url

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ cd "$(dirname "$0")"
 python3 -m venv venv
 source venv/bin/activate
 
-python3 -m pip install .[required] .[test] .[dev]
+python3 -m pip install .[requirements] .[requirements_test] .[requirements_dev]
