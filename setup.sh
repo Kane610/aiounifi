5d57954c93c6aa9d2355bb1c788a5d4e177b37c2
Robert Svensson <Kane610@users.noreply.github.com>
2024-12-07 09:11:27 +0100

    Fix pip reporting optional dependencies with '_' as non existing

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ cd "$(dirname "$0")"
 python3 -m venv venv
 source venv/bin/activate
 
-python3 -m pip install ".[requirements, requirements_test, requirements_dev]"
+python3 -m pip install ".[requirements, requirements-test, requirements-dev]"
